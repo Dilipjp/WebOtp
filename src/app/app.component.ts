6eb5f38df0ec2ba2dabcb5b329dc4a74e6173f74
Dilip <dilip@Dilips-Mac-mini.local>
2022-03-03 10:57:45 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  mainObj:any={};
+
+  constructor() {}
+
+
+  myOTP:any;
+
+  ngAfterViewInit() {
+    if ('OTPCredential' in window) {
+        this.mainObj.isWebOtpSupported = true;
+        debugger;
+        window.addEventListener('DOMContentLoaded', e => {
+            debugger;
+        const input = document.querySelector('input[autocomplete="one-time-code"]');
+        if (!input) return;
+        const ac = new AbortController();
+        const form = input.closest('form');
+        debugger;
+        if (form) {
+            debugger;
+            form.addEventListener('submit', e => {
+            ac.abort();
+            });
+        }
+        var reqObj =  {
+          newotp: 
+          otp: { transport:['sms'] },
+          signal: ac.signal
+      };
+        navigator.credentials.get(
+          reqObj
+        ).then((otp:any) => {
+            debugger;
+            if(
+              otp
+            ){
+              if(
+                otp && otp.code
+              ){
+                // input.value = otp.code;
+                this.myOTP = otp.code;
+              }
+            }
+            
+            // if (form) form.submit();
+        }).catch(err => {
+            debugger;
+            console.log(err);
+        });
+        });
+    }else{
+      // this.myOTP = 521456;
+      this.mainObj.isWebOtpSupported = false;
+      // alert('Web OTP API not supported, Please enter manually.');
+    }
+  }
+}
